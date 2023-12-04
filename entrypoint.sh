704f090a67ad455cef7d7f758f4ead6e96a05ab7
Minami Ueda <minami.ueda@gmail.com>
2023-12-04 20:42:45 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ apt-get update -y
 apt-get install -y wget 
 wget http://ftp.de.debian.org/debian/pool/contrib/m/msttcorefonts/ttf-mscorefonts-installer_3.8.1_all.deb
 apt install -y ./ttf-mscorefonts-installer_3.8.1_all.deb
+# Reset font cache
+fc-cache -f -v
 
 tex_files="${1}"
 compiler="${2}"
